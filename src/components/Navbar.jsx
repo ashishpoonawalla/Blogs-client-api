b58e56cda2ed76e93826d3de2a13c2fd3aad1449
Ashish <ashish.omar.pune@gmail.com>
2022-12-28 11:11:00 +0530

    Scss Setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import Logo from "../img/logo.png"
+import {Link} from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="logo">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="links">
+        <Link to="/?cat=art" className="link">
+          <h6>ART</h6>
+        </Link>
+        <Link to="/?cat=art" className="link">
+          <h6>ART</h6>
+        </Link>
+        <Link to="/?cat=art" className="link">
+          <h6>ART</h6>
+        </Link>
+        <Link to="/?cat=art" className="link">
+          <h6>ART</h6>
+        </Link>
+        <Link to="/?cat=art" className="link">
+          <h6>ART</h6>
+        </Link>
+        <Link to="/?cat=art" className="link">
+          <h6>ART</h6>
+        </Link>
+        <Link to="/?cat=art" className="link">
+          <h6>ART</h6>
+        </Link>
+      </div>
+    </div>
   )
 }
 
