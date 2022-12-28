c3f54e7f22be9a25e2cdfbe7b112f4ed9bbd6a6f
Ashish <ashish.omar.pune@gmail.com>
2022-12-28 12:35:17 +0530

    Footer UI Design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import Logo from "../img/logo.png"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="container">
+        <div className="item">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="item">Spread ❤️ to my blogs. &copy; 2022</div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,24 +12,28 @@ const Navbar = () => {
 					<Link to="/?cat=art" className="link">
 						<h6>ART</h6>
 					</Link>
-					<Link to="/?cat=art" className="link">
-						<h6>ART</h6>
+					<Link to="/?cat=science" className="link">
+						<h6>SCIENCE</h6>
 					</Link>
-					<Link to="/?cat=art" className="link">
-						<h6>ART</h6>
+					<Link to="/?cat=technology" className="link">
+						<h6>TECHNOLOGY</h6>
 					</Link>
-					<Link to="/?cat=art" className="link">
-						<h6>ART</h6>
+					<Link to="/?cat=cinema" className="link">
+						<h6>CINEMA</h6>
 					</Link>
-					<Link to="/?cat=art" className="link">
-						<h6>ART</h6>
+					<Link to="/?cat=design" className="link">
+						<h6>DESIGN</h6>
 					</Link>
-					<Link to="/?cat=art" className="link">
-						<h6>ART</h6>
+					<Link to="/?cat=food" className="link">
+						<h6>FOOD</h6>
 					</Link>
-					<Link to="/?cat=art" className="link">
-						<h6>ART</h6>
-					</Link>
+					<span>jhon</span>
+					<span>logout</span>
+					<span className="write">
+						<Link to="/write" className="link">
+							Write
+						</Link>
+					</span>
 				</div>
 			</div>
 		</div>
