2608966086ec05118861f4210e5a25cc3a1c8441
Ashish <ashish.omar.pune@gmail.com>
2022-12-29 22:49:04 +0530

    API Init

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react"
 import {Link} from "react-router-dom"
 
 const Register = () => {
+  const [inputs, setInputs] = useState({})
+
+  const handleChange = (e) => {
+    setInputs(prev=> ({...prev, [e.target.name]: e.target.value}))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+  }
+
+
+  console.log(inputs)
   return (
     <div className='auth'>
     <h1>Sign up</h1>
     <form>
-      <input type="text" placeholder='username' />
-      <input type="email" placeholder='email' />
-      <input type="password" placeholder='password' />
-      <button>Sign up</button>
+      <input type="text" placeholder='username' name='username' onChange={handleChange}/>
+      <input type="email" placeholder='email'  name='email' onChange={handleChange}/>
+      <input type="password" placeholder='password'  name='password' onChange={handleChange}/>
+      <button onClick={handleSubmit}>Sign up</button>
       <span>Wrong credential!</span>
       <p>already have account? <Link to="/login">Login</Link></p>
     </form>
