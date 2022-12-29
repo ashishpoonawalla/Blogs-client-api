ce8cc9c2e18562213bd2277bcf523f3de7b74219
Ashish <ashish.omar.pune@gmail.com>
2022-12-29 23:09:17 +0530

    Register API Done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,34 +1,64 @@
-import { useState } from "react"
-import {Link} from "react-router-dom"
+import {useState} from "react"
+import {Link, useNavigate} from "react-router-dom"
+import axios from "axios"
 
 const Register = () => {
-  const [inputs, setInputs] = useState({})
+	const [inputs, setInputs] = useState({
+		username: "",
+		email: "",
+		password: "",
+	})
+	const [err, setError] = useState(null)
 
-  const handleChange = (e) => {
-    setInputs(prev=> ({...prev, [e.target.name]: e.target.value}))
-  }
+	const navigate = useNavigate()
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    
-  }
+	const handleChange = (e) => {
+		setInputs((prev) => ({...prev, [e.target.name]: e.target.value}))
+	}
 
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		try {
+			await axios.post("auth/register", inputs)
+			navigate("/login")
+		} catch (err) {
+			setError(err.response.data)
+		}
+	}
 
-  console.log(inputs)
-  return (
-    <div className='auth'>
-    <h1>Sign up</h1>
-    <form>
-      <input type="text" placeholder='username' name='username' onChange={handleChange}/>
-      <input type="email" placeholder='email'  name='email' onChange={handleChange}/>
-      <input type="password" placeholder='password'  name='password' onChange={handleChange}/>
-      <button onClick={handleSubmit}>Sign up</button>
-      <span>Wrong credential!</span>
-      <p>already have account? <Link to="/login">Login</Link></p>
-    </form>
-    
-  </div>
-  )
+	return (
+		<div className="auth">
+			<h1>Sign up</h1>
+			<form>
+        <input
+          required
+					type="text"
+					placeholder="username"
+					name="username"
+					onChange={handleChange}
+				/>
+        <input
+          required
+					type="email"
+					placeholder="email"
+					name="email"
+					onChange={handleChange}
+				/>
+        <input
+          required
+					type="password"
+					placeholder="password"
+					name="password"
+					onChange={handleChange}
+				/>
+				<button onClick={handleSubmit}>Sign up</button>
+				{err && <p>{err}</p>}
+				<p>
+					already have account? <Link to="/login">Login</Link>
+				</p>
+			</form>
+		</div>
+	)
 }
 
 export default Register
